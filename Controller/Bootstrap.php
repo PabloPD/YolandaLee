896ae11ef77bb2579bf2947cce18465a36d4260c
Pablo <Pablo@192.168.1.15>
2015-06-16 21:34:35 +0200

    5. Create .htacces, update class bootstrap

--- Controller/Bootstrap.php
+++ Controller/Bootstrap.php
@@ -14,6 +14,40 @@
 class Bootstrap {
     //put your code here
     public function __construct() {
+
+            if (isset($_GET['url'])) {
+
+                $url = $_GET['url'];
+                $url = rtrim($url, '/');
+                $url = explode('/', $url);
+
+                $file = 'Controller/' . $url[0] . '.php';
+
+                if (file_exists($file)) {
+
+                    require $file;
+                    $controller = new $url[0];
+
+                    if (!isset($url[1])) {
+                        $controller->goIndex();
+                    } elseif (isset($url[1])) {
+
+                        $controller->{$url[1]}();
+                    }
+                } else {
+                    // This redirect to main page 
+                    header("Location: .");
+                    die();
+                }
+            } else {
+                $file = 'Controller/View.php';
+                if (file_exists($file)) {
+                    require_once $file;
+                    $controller = new View();
+                    $controller->goIndex();
+                } else
+                    echo "View.php doesn't exists";
+            }
         
     }
 }
