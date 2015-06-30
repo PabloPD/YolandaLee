207970af389bbd1fd05421067a8a7c033f4ce584
Pablo <Pablo@192.168.1.15>
2015-06-30 20:32:04 +0200

    13. Create class Log.

---
+++ Model/Log.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Log
+ *
+ * @author Pablo
+ */
+class Log {
+    //put your code here
+    public function __construct() {
+        
+    }
+    
+    function missage($message){
+        
+        //date_default_timezone_set('UTC');
+        $date= date('l jS \of F Y h:i:s A');
+
+        $file = 'php_error.log';
+        if(file_exists($file)){
+            error_log($date.' : '.$message.' *****', 3, $file);
+        }
+        else    echo 'no existe fichero php_errors.log';
+    }
+    
+    function fileNotFound($message){
+        
+        //date_default_timezone_set('UTC');
+        $date= date('l jS \of F Y h:i:s A');
+
+        $file = 'php_error.log';
+        if(file_exists($file)){
+            error_log($date.' : '.$message.' *****', 3, $file);
+        }
+        else    echo 'no existe fichero php_errors.log';
+    }
+}
--- Server/Functions/webservice_functions.php
+++ Server/Functions/webservice_functions.php
@@ -12,8 +12,7 @@ require_once('Functions/global_functions.php');
  */
 function getbooks($email, $password) {
     $dbo = (new ModelPDO())->getDBO();  // Database Object
-    // Get role from user if exists
-    //$password = '62c8ad0a15d9d1ca38d5';
+
     $sth = $dbo->prepare("select b_valoracion, b_comentario, b_picture, ti_name, au_name from book left join titulo on b_ti_fk=ti_id left join autor on ti_au_fk_autor=au_id");
     // Set parameters
     //$sth->execute(array($email, $password));
